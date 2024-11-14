8cce034535f78169aae8e255a4218181fc1ab7ad
Mansab Mir <mansabmirsde@gmail.com>
2024-11-14 12:38:24 +0530

    Digiexpo website initialised

---
+++ next.config.mjs
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    // Unoptimized images setting for static export (if using a CDN, consider optimized images)
+    unoptimized: true,
+  },
+  productionBrowserSourceMaps: true,
+  compress: true,
+  swcMinify: true,
+
+};
+
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   reactStrictMode: true,
+//   images: {
+//       unoptimized: true,
+//     },
+// };
+
+// export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   reactStrictMode: true,
+//   images: {
+//       unoptimized: true,
+//     },
+// }
+
+// module.exports = nextConfig;
