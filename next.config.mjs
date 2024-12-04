6d58b96610412fe3c812bab568b41261e0fc839b
Mansab Mir <mansabmirsde@gmail.com>
2024-12-04 19:15:28 +0530

    Sitemap and robots added

--- next.config.mjs
+++ next.config.mjs
@@ -12,25 +12,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   reactStrictMode: true,
-//   images: {
-//       unoptimized: true,
-//     },
-// };
-
-// export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   reactStrictMode: true,
-//   images: {
-//       unoptimized: true,
-//     },
-// }
-
-// module.exports = nextConfig;
