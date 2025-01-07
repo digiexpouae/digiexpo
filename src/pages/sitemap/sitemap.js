f0661bb394ebf8ab2e29ceb4cafef423b0c8de29
ramsha1993 <rimshasaleem1993@gmail.com>
2025-01-07 19:24:59 +0500

    slug

--- src/pages/sitemap/sitemap.js
+++ src/pages/sitemap/sitemap.js
@@ -2,25 +2,22 @@ import client from "@/sanityConfig";
 
 const BASE_URL='https://www.digiexpo.ae/'
 export const query = `*[_type == "post" && slug.current == $slug][0]{
-  title,
+  
   "currentSlug": slug.current,
-  body,
-  mainImage,ogImage,ogDescription,
-  ogTitle,metaTags,description
+
 }`;
-export async function sitemap(){
+export async function sitemap(slug){
   try{
-const post=await client.fetch(query)
+const post=await client.fetch(query, {slug})
 const urls=post.map((item)=>{
   
   return `${BASE_URL}/item/${item.currentSlug}}`
 
 })
-return urls
+return [urls]
   }catch (error) {
     console.error("Error fetching data for sitemap:", error);
     return [];
 
   }
 }
-sitemap()
