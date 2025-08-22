5980b8151c14d007264dfaff4df983bca1cf2456
ramsha1993 <rimshasaleem1993@gmail.com>
2025-08-22 15:46:29 +0500

    optimize

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,10 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          href='https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Montserrat+Alternates:wght@300;400;500;600;700;800&family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&family=Urbanist:wght@300;400;500;600;700;800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap'
-          rel='stylesheet'
-        />
+    
       </Head>
       <body>
         <Main />
