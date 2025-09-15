85e1c63f852f2407f3b4ef9baf0596bb3da7f39e
ramsha1993 <rimshasaleem1993@gmail.com>
2025-09-15 18:07:23 +0500

    logo

--- src/forms/Contactusprovider.jsx
+++ src/forms/Contactusprovider.jsx
@@ -1,11 +1,11 @@
 import ContactUsForm from "./contact-us-form";
 import dynamic from "next/dynamic";
-const RecaptchaWrapper = dynamic(() => import("../components/Provider/googlerecaptchaprovider"), { ssr: false });const contactmuzz=()=>{
-const contactmuzz=()=>{
+const RecaptchaWrapper = dynamic(() => import("../components/Provider/googlerecaptchaprovider"), { ssr: false })
+const contactus=()=>{
     return(
 <RecaptchaWrapper>
     <ContactUsForm />
 </RecaptchaWrapper>
     )
 }
-export default contactmuzz
+export default contactus
--- src/forms/cotanct-muzz.jsx
+++ src/forms/cotanct-muzz.jsx
@@ -1,6 +1,7 @@
 import ContactUsFormMuz from "./contact-us-form-muz";
 import dynamic from "next/dynamic";
-const RecaptchaWrapper = dynamic(() => import("../components/Provider/googlerecaptchaprovider"), { ssr: false });const contactmuzz=()=>{
+const RecaptchaWrapper = dynamic(() => import("../components/Provider/googlerecaptchaprovider"), { ssr: false });
+const contactmuzz=()=>{
     return(
 <RecaptchaWrapper>
     <ContactUsFormMuz/>
