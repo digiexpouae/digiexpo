6f2f0ecd15ce91774715568e41a0a760eac7dad9
ramsha1993 <rimshasaleem1993@gmial.com>
2025-11-13 15:23:54 +0500

    lottie

--- src/components/lottie.jsx
+++ src/components/lottie.jsx
@@ -1,12 +1,12 @@
 // Import only the `loadAnimation` method from `lottie-web`
-import lottie from 'lottie-web/build/player/lottie_svg.min.js';
+import { loadAnimation } from 'lottie-web/build/player/lottie_light';
 import { useEffect, useRef } from 'react';
 
 const LottieComponent = ({ animationData, width , height = '130px' }) => {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    const animation = lottie.loadAnimation({
+    const animation =loadAnimation({
       container: containerRef.current,
       animationData,
       loop: true,
