cbd0b34fb0e6d5e557f88fd31a549a6ebcb111c2
ramsha1993 <rimshasaleem1993@gmail.com>
2026-01-21 11:09:42 +0500

    banners updated

--- src/components/Empiredevelopments/empire.jsx
+++ src/components/Empiredevelopments/empire.jsx
@@ -1,79 +1,79 @@
 import React from 'react'
 import Herosection from './herosection'
-import Header from "@/layout/headers/header-3"
+import Header from "@/layout/headers/header-2"
 import Footer from "@/layout/footers/footer-4"
 import Section8 from '../about2/section8'
 import Section from '../about2/section2'
- import ContactArea from '../../components/homes/home/contact-area'
+import ContactArea from '../../components/homes/home/contact-area'
 import Section2 from './section2'
 import Section3 from './section3'
 import Section4 from '../about2/section7'
 import Section5 from './section4'
 import Table from './table'
-import Counter from  './maincounter'
+import Counter from './maincounter'
 import Challenges from './challenges'
 import Delivered from './delivered'
 import Client from './aboutclient'
 const empire = () => {
-  const items= [
-  {
-    title: "Instill luxury developments with visually appealing images.",
-  },
-  {
-    title: "Enhance interaction through easy navigation.",
-  },
-  {
-    title: "Ensure quick loading and high level of security.",
-  },
-  {
-    title: "Present the information in a clear manner with optimized information.",
-  },
-  {
-    title: "Reflect the Empire Developments vision along with its sophistication and confidence.",
-  }
-];
-const properties = [
-  {
-    id: 1,
-    title: 'Attractive Design',
-  },
-  {
-    id: 2,
-    title: 'Optimized Site Speed',
-  },
-  {
-    id: 3,
-    title: 'Advanced Security',
-  },
-  {
-    id: 4,
-    title: 'Engaging Content',
-  },
-   {
-    id: 3,
-    title: 'Responsive Website',
-  },
-  {
-    id: 4,
-    title: 'Accessibility',
-  },
-];
-  const desc='DigiExpo followed the four-stage process of changing the online presence of Empire Developments.'
-    const steps = [
-        {
-          title: "Discovery & Strategy",
-        },
-        {
-          title: "UX/UI Design",
-        },
-        {
-          title: "Development",
-        },
-        {
-          title: "Testing & Launch",
-        },
-      
-      ];
+  const items = [
+    {
+      title: "Instill luxury developments with visually appealing images.",
+    },
+    {
+      title: "Enhance interaction through easy navigation.",
+    },
+    {
+      title: "Ensure quick loading and high level of security.",
+    },
+    {
+      title: "Present the information in a clear manner with optimized information.",
+    },
+    {
+      title: "Reflect the Empire Developments vision along with its sophistication and confidence.",
+    }
+  ];
+  const properties = [
+    {
+      id: 1,
+      title: 'Attractive Design',
+    },
+    {
+      id: 2,
+      title: 'Optimized Site Speed',
+    },
+    {
+      id: 3,
+      title: 'Advanced Security',
+    },
+    {
+      id: 4,
+      title: 'Engaging Content',
+    },
+    {
+      id: 3,
+      title: 'Responsive Website',
+    },
+    {
+      id: 4,
+      title: 'Accessibility',
+    },
+  ];
+  const desc = 'DigiExpo followed the four-stage process of changing the online presence of Empire Developments.'
+  const steps = [
+    {
+      title: "Discovery & Strategy",
+    },
+    {
+      title: "UX/UI Design",
+    },
+    {
+      title: "Development",
+    },
+    {
+      title: "Testing & Launch",
+    },
+
+  ];
   const features = [
     {
       title: "Modern and Attractive Layout",
@@ -98,42 +98,42 @@ const properties = [
   ];
 
 
-      const text='Empire Developments is a leading UAE-based real estate developer known for developing luxury residential and commercial real estate. Having a strong emphasis on innovation and design excellence, they have created new standards in the real estate business by their luminous projects and architectural genius.'
+  const text = 'Empire Developments is a leading UAE-based real estate developer known for developing luxury residential and commercial real estate. Having a strong emphasis on innovation and design excellence, they have created new standards in the real estate business by their luminous projects and architectural genius.'
 
   return (
     <div>
-  <Header />
-  <Herosection image={"/assets/img/casestudies/Empire copy.png"} text1={<>Empire <br />Developments</>} text2={text}/> 
-   <Table
-   text={'DigiExpo developed a smooth and glamorous site as requested by Empire Developments. The website exemplifies their high-end real estate brand and highlights their luxurious developments with sophistication.'}
-   title={'Empire Developments'}
-srclink={'/assets/empire_small.mp4'}
-textone={'Real Estate'}
-titletwo={'Technologies'}
-texttwo={'Website Development'}
-titlethree={'Launched'}
-textthree={'2025'}
-titlefour={'Country'}
-textfour={'UAE'}
-    />
-  {/* <Section2 text={'Empire Developments is a premier real estate developer known for creating iconic, luxury residential and commercial properties. With a reputation for excellence and innovation, they sought a digital partner to build a website that reflected their premium brand and could effectively market high-value projects to an elite audience.'}/> */}
- <Challenges features={features}/>
-<Client srclink={"/assets/img/casestudies/Empire copy.png"} title={<>Experience the Art <br /> of Modern Real Estate</>} text={'Empire Developments represents one of the best luxury real estate developments in the UAE, and has the best residential and commercial developments in the world. They required an online platform to present their work gracefully and accurately.'}
-titleone={'Our Services For Empire Developments.'}
-textone={'DigiExpo designed a modern and attractive website for Empire Developments. The purpose of this website was to present a contemporary image of the brand and its luxury. The development of the site focused on providing high-quality visuals, navigation, and proper presentation of their projects. Every aspect of design and features was expounded to highlight their position as a high-end real estate developer.'}
-/>
- <Delivered properties={properties} title={'What We Delivered'} text={'Empire Developments now has a modern and responsive SEO friendly website that was designed and delivered by our web development team. Their luxury real estate identity is represented in a very beautiful way by the new design and offers a perfect user experience.'} items={items}
- 
- heading={'The website was built to:'}
- />
-  <Counter />
-  {/* <Section3 /> */}
-  {/* <Section className={'!tw-hidden'} /> */}
-  <Section4 maintitle={'Process'} classhead={'!tw-mb-3'} text={true} desc={desc} steps={steps} className={'!tw-grid-cols-2  lg:!tw-grid-cols-2 md:!tw-w-[60%]'} />
-  <Section5 />
-  <Section8 />
-  <ContactArea />
-  <Footer />
+      <Header />
+      <Herosection image={"/assets/img/casestudies/Empire copy.png"} imagemob={"/assets/img/herowebp/empiredevelopment.png"} text1={<>Empire <br />Developments</>} text2={text} bgVariant="empire" />
+      <Table
+        text={'DigiExpo developed a smooth and glamorous site as requested by Empire Developments. The website exemplifies their high-end real estate brand and highlights their luxurious developments with sophistication.'}
+        title={'Empire Developments'}
+        srclink={'/assets/empire_small.mp4'}
+        textone={'Real Estate'}
+        titletwo={'Technologies'}
+        texttwo={'Website Development'}
+        titlethree={'Launched'}
+        textthree={'2025'}
+        titlefour={'Country'}
+        textfour={'UAE'}
+      />
+      {/* <Section2 text={'Empire Developments is a premier real estate developer known for creating iconic, luxury residential and commercial properties. With a reputation for excellence and innovation, they sought a digital partner to build a website that reflected their premium brand and could effectively market high-value projects to an elite audience.'}/> */}
+      <Challenges features={features} />
+      <Client srclink={"/assets/img/casestudies/Empire copy.png"} title={<>Experience the Art <br /> of Modern Real Estate</>} text={'Empire Developments represents one of the best luxury real estate developments in the UAE, and has the best residential and commercial developments in the world. They required an online platform to present their work gracefully and accurately.'}
+        titleone={'Our Services For Empire Developments.'}
+        textone={'DigiExpo designed a modern and attractive website for Empire Developments. The purpose of this website was to present a contemporary image of the brand and its luxury. The development of the site focused on providing high-quality visuals, navigation, and proper presentation of their projects. Every aspect of design and features was expounded to highlight their position as a high-end real estate developer.'}
+      />
+      <Delivered properties={properties} title={'What We Delivered'} text={'Empire Developments now has a modern and responsive SEO friendly website that was designed and delivered by our web development team. Their luxury real estate identity is represented in a very beautiful way by the new design and offers a perfect user experience.'} items={items}
+
+        heading={'The website was built to:'}
+      />
+      <Counter />
+      {/* <Section3 /> */}
+      {/* <Section className={'!tw-hidden'} /> */}
+      <Section4 maintitle={'Process'} classhead={'!tw-mb-3'} text={true} desc={desc} steps={steps} className={'!tw-grid-cols-2  lg:!tw-grid-cols-2 md:!tw-w-[60%]'} />
+      <Section5 />
+      <Section8 />
+      <ContactArea />
+      <Footer />
     </div>
   )
 }
--- src/components/Empiredevelopments/herosection.jsx
+++ src/components/Empiredevelopments/herosection.jsx
@@ -1,22 +1,45 @@
 import Image from "next/image";
 import Link from 'next/link'
-const HeroSection = ({ text1, text2, image }) => {
+const HeroSection = ({ text1, text2, image, imagemob, bgVariant }) => {
   const title = text1
 
   const text = text2
+
+
+  const BG_VARIANTS = {
+    empire: `
+    tw-bg-[url('/assets/img/herowebp/empiredevelopment.webp')]
+    md:tw-bg-[url('/assets/img/herowebp/casestudy-bg.webp')]
+  `,
+    kamdar: `
+    tw-bg-[url('/assets/img/herowebp/kamdar.webp')]
+    md:tw-bg-[url('/assets/img/herowebp/casestudy-bg.webp')]
+  `,
+    ocean: `
+    tw-bg-[url('/assets/img/herowebp/ocean.webp')]
+    md:tw-bg-[url('/assets/img/herowebp/casestudy-bg.webp')]
+  `,
+    saami: `
+    tw-bg-[url('/assets/img/herowebp/saami.webp')]
+    md:tw-bg-[url('/assets/img/herowebp/casestudy-bg.webp')]
+  `,
+
+    // add more variants here
+  };
   return (
     <div
-      className="tw-h-screen md:tw-h-[90vh] md:tw-rounded-bl-[80px]  md:tw-rounded-br-[80px] tw-w-full tw-bg-cover tw-bg-center tw-flex tw-flex-col lg:tw-flex-row tw-items-center md:tw-pt-20 tw-justify-center  tw-relative"
-      style={{ backgroundImage: "url('/assets/img/about2/ab-2.png')" }}
+      className={`tw-h-screen md:tw-h-screen md:tw-rounded-bl-[80px]  md:tw-rounded-br-[80px] tw-w-full tw-bg-cover tw-bg-center tw-flex tw-flex-col lg:tw-flex-row tw-items-center md:tw-pt-20 tw-justify-center         ${BG_VARIANTS[bgVariant]}
+    tw-bg-cover tw-bg-center tw-relative`}
+
     >
       {/* Inner content */}
-      <div className=" lg:tw-w-1/2   lg:tw-h-full tw-flex tw-flex-col lg:tw-pl-24 tw-justify-center tw-pt-10 md:tw-py-0 tw-px-12">
-        <h1 className="tw-text-black tw-text-3xl md:tw-text-5xl tw-font-bold ">
+      <div className=" lg:tw-w-1/2   lg:tw-h-full tw-flex tw-flex-col lg:tw-pl-24 tw-justify-center tw-mb-10 md:tw-py-0 tw-px-4">
+        <h1 className="tw-text-white tw-text-center  md:tw-text-start tw-text-3xl md:tw-text-5xl tw-font-bold ">
           {title}
         </h1>
-        <p className="">{text}</p>
+        <p className="tw-text-white tw-text-center md:tw-text-start tw-text-xs md:tw-text-base  ">{text}</p>
 
-        <div className="tw-flex tw-gap-4">
+        {/* <div className="tw-flex tw-gap-4">
           <Link
             href="/casestudies"
             style={{ whiteSpace: 'nowrap' }}
@@ -32,20 +55,22 @@ const HeroSection = ({ text1, text2, image }) => {
           >
             Contact Us
           </Link>
-        </div>
+        </div> */}
       </div>
 
       {/* Hero image */}
       <div className="tw-flex tw-h-[35%] tw-w-full  lg:tw-h-full tw-items-end  tw-justify-end lg:tw-w-1/2 tw-translate-x-[10%]">
 
-        <div className="  tw-h-[80%] tw-relative tw-w-full" > <Image
+        <div className="  tw-h-full  tw-hidden md:tw-block  tw-relative tw-w-full" > <Image
           src={image}
           fill
-          className="tw-object-cover tw-rounded-2xl "
-          alt="Hero Visual"
+          className="tw-object-cover  "
+        // alt="Hero Visual"
         />
-          <div className="tw-absolute tw-h-full tw-right-0 tw-w-[60%] tw-blur-xl tw-bg-[linear-gradient(180deg,rgba(255,255,255,0)_65.78%,#FFFFFF_100%),linear-gradient(90deg,rgba(255,255,255,0)_62.72%,#FFFFFF_84.98%)]  tw-z-20" ></div>
-          <div className="tw-absolute tw-h-[30%] tw-bottom-0 tw-right-0 tw-w-full tw-blur-xl tw-bg-[linear-gradient(180deg,rgba(255,255,255,0)_65.78%,#FFFFFF_100%),linear-gradient(90deg,rgba(255,255,255,0)_62.72%,#FFFFFF_84.98%)]  tw-z-20" ></div>
+
+
+          {/* <div className="tw-absolute tw-h-full tw-right-0 tw-w-[60%] tw-blur-xl tw-bg-[linear-gradient(180deg,rgba(255,255,255,0)_65.78%,#FFFFFF_100%),linear-gradient(90deg,rgba(255,255,255,0)_62.72%,#FFFFFF_84.98%)]  tw-z-20" ></div> */}
+          {/* <div className="tw-absolute tw-h-[30%] tw-bottom-0 tw-right-0 tw-w-full tw-blur-xl tw-bg-[linear-gradient(180deg,rgba(255,255,255,0)_65.78%,#FFFFFF_100%),linear-gradient(90deg,rgba(255,255,255,0)_62.72%,#FFFFFF_84.98%)]  tw-z-20" ></div> */}
 
         </div>
       </div>
